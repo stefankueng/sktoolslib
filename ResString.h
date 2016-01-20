2b2b92ca654f66538a7636529f8cabc4199020cb
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2016-01-20 18:45:08 +0000

    Patch from gmisocpp: Improve the resource string handling so it requires less ugly casts in some situations and is more usable in others and more efficient in time/space. Resource strings now should take a bit less memory and avoid any excess reallocation.

--- ResString.h
+++ ResString.h
@@ -1,6 +1,6 @@
 // sktoolslib - common files for SK tools
 
-// Copyright (C) 2013 - Stefan Kueng
+// Copyright (C) 2013, 2016 Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -19,34 +19,30 @@
 
 #pragma once
 #include <string>
-#include <memory>
 
 /**
  * Loads a string from the application resources.
  */
+
+inline std::wstring LoadResourceWString(HINSTANCE hInstance, UINT uID)
+{
+    const wchar_t* p = nullptr;
+    int len = ::LoadStringW(hInstance, uID, reinterpret_cast<LPWSTR>(&p), 0);
+    return (len > 0) ? std::wstring(p, static_cast<size_t>(len)) : std::wstring();
+}
+
 class ResString
 {
 public:
     // hInst is necessary to support multiple languages with resource dlls
-    ResString (HINSTANCE hInst, int resId)
+    inline ResString (HINSTANCE hInst, int resId)
+        : str(LoadResourceWString(hInst, resId))
     {
-        int bufsize = 1024;
-        for ( ;; )
-        {
-            std::unique_ptr<wchar_t[]> buf(new wchar_t[bufsize]);
-            int ret = ::LoadString(hInst, resId, buf.get(), bufsize);
-            if (ret == (bufsize-1))
-                bufsize *= 2;
-            else
-            {
-                str = buf.get();
-                break;
-            }
-        }
     }
-    operator TCHAR const * () const { return str.c_str(); }
-    operator std::wstring () const { return str; }
+    // Handy for when used with printf etc and to avoid explicit casts.
+    inline const TCHAR* c_str() const { return str.c_str(); }
+    inline operator const TCHAR*const () const { return str.c_str(); }
+    inline operator const std::wstring& () const { return str; }
 private:
-    std::wstring str;
+    const std::wstring str;
 };
-
