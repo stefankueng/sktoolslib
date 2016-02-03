f5e03a89a189f08821af1b7eaebef07fde83ade1
steveking <steveking@e077847a-e44e-4671-b95f-5f716b05e630>
2016-02-03 20:53:39 +0000

    Fix copyright year.

--- trunk/BaseDialog.cpp
+++ trunk/BaseDialog.cpp
@@ -1,6 +1,6 @@
 // sktoolslib - common files for SK tools
 
-// Copyright (C) 2012-2013, 2015 - Stefan Kueng
+// Copyright (C) 2012-2013, 2015-2016 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
