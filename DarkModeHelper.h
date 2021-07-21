a22bd53d7f59e5f64889c4d400fb91e394c2529f
Stefan Küng <stefankueng@gmail.com>
2021-07-21 10:31:18 +0200

    remove unused using

--- DarkModeHelper.h
+++ DarkModeHelper.h
@@ -106,7 +106,6 @@ private:
     using RefreshImmersiveColorPolicyStateFn = void(WINAPI* )();
     using GetIsImmersiveColorUsingHighContrastFn = BOOL(WINAPI* )(IMMERSIVE_HC_CACHE_MODE mode);
     using FlushMenuThemesFn = void(WINAPI* )();
-    using OpenNcThemeDataFpn = HTHEME(WINAPI* )(HWND hWnd, LPCWSTR pszClassList);
     using SetWindowCompositionAttributeFpn = BOOL(WINAPI* )(HWND hwnd, WINDOWCOMPOSITIONATTRIBDATA* data);
 
     AllowDarkModeForAppFpn                 m_pAllowDarkModeForApp                  = nullptr;
